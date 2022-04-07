12ad460c1008da89bb5446acf551867a117aad19
CodeOpFE <codeopfe@MacBook-Air-de-CodeOpFE-8.local>
2022-04-07 13:57:04 +0200

    projecto

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,71 +1,73 @@
 <template>
-  <div>
-    <div
-      class="relative h-96 w-full bg-gradient-to-tr from-gray-700 to-amber-700 bg-cover bg-center"
-    >
-      <img
-        src="https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1980&q=80')]"
-        alt="bookshelf"
-        class="absolute h-full w-full object-cover mix-blend-overlay"
-      />
-      <div class="flex flex-col items-center px-2 py-3">
-        <h1 class="text-xl font-bold text-white sm:text-4xl md:text-6xl">
+  <div class="bg-teal-100">
+    <div class="container mx-auto">
+      <!-- <img
+      src="https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1980&q=80')]"
+      alt="bookshelf"
+      class="absolute h-full w-full object-cover mix-blend-overlay"
+    /> -->
+      <div class="flex flex-col bg-black px-2 py-3 text-justify">
+        <h1
+          class="xs:text-2xl text-xl font-bold text-white sm:text-4xl md:text-6xl"
+        >
           GUIOS-READS
         </h1>
-        <h2 class="text-lg font-light italic text-white sm:text-xl md:text-3xl">
+        <h2 class="text-sd font-light italic text-white sm:text-xl md:text-3xl">
           "A room without books is like a body without a soul"
         </h2>
-        <div class="flex flex-col justify-center gap-3 py-2 text-center">
+        <div class="flex flex-col items-center justify-center gap-3 py-2">
           <div
-            class="border-b-2 border-dashed border-amber-200 pr-2 text-lg leading-loose tracking-wide text-white sm:text-3xl"
+            class="border-b-2 border-dashed border-amber-200 pr-2 text-lg leading-loose tracking-wide text-white sm:text-2xl"
           >
             READ
           </div>
           <div
-            class="ttext-lg border-b-2 border-dashed border-amber-200 pr-2 leading-loose tracking-wide text-white sm:text-3xl"
+            class="ttext-lg border-b-2 border-dashed border-amber-200 pr-2 leading-loose tracking-wide text-white sm:text-2xl"
           >
             READING
           </div>
           <div
-            class="text-lg leading-loose tracking-wide text-white sm:text-3xl"
+            class="text-lg leading-loose tracking-wide text-white sm:text-2xl"
           >
             WISH
           </div>
         </div>
       </div>
     </div>
-  </div>
-  <div class="flex flex-col pb-4">
-    <h3 class="italic text-amber-600">Find your wish book:</h3>
-    <div>
-      <!-- ref pasarlo a <v-model> -->
-      <input
-        type="text"
-        class="mt-2 rounded-md border-2 border-amber-700 py-1 md:px-8"
-        placeholder="title or author"
-        ref="searchInput"
-        @keyup.enter="getBooks()"
-      />
-      <button
-        class="mt-3 rounded-md bg-amber-600 py-1 px-1 hover:bg-orange-400"
-        @click="getBooks()"
-      >
-        Q
-      </button>
+
+    <div class="flex flex-col items-center pb-4">
+      <h3 class="italic text-amber-600">Find your wish book:</h3>
+      <div>
+        <!-- ref pasarlo a <v-model> -->
+        <input
+          type="text"
+          class="mt-2 rounded-md border-2 border-amber-700 py-1 md:px-8"
+          placeholder="title or author"
+          ref="searchInput"
+          @keyup.enter="getBooks()"
+        />
+        <button
+          class="mt-3 rounded-md bg-amber-600 py-1 px-1 hover:bg-orange-400"
+          @click="getBooks()"
+        >
+          Q
+        </button>
+      </div>
     </div>
-  </div>
-  <div class="grid grid-cols-2 bg-gray-500 px-8">
+
     <div v-if="bookInfo">
-      <div v-for="book in bookInfo.items" :key="book.id">
-        <div class="bg-teal-100 px-4">
-          <p clas>title: {{ book.volumeInfo.title }}</p>
-          <div v-for="author in book.volumeInfo.authors" :key="author">
-            <p>autor: {{ author }}</p>
-          </div>
-          <p>description: {{ book.volumeInfo.description }}</p>
-          <br />
+      <div class="container mx-auto grid grid-cols-3 gap-2 bg-gray-500">
+        <div v-for="book in bookInfo.items" :key="book.id">
           <div v-for="image in book.volumeInfo.imageLinks" :key="image">
-            <img :src="image" alt="books images" />
+            <img :src="image" alt="books images" class="h-24" />
+            <div class="bg-orange-100 text-justify text-xs">
+              <p>{{ book.volumeInfo.title }}</p>
+              <div v-for="author in book.volumeInfo.authors" :key="author">
+                <p>autor: {{ author }}</p>
+              </div>
+              <p>description: {{ book.volumeInfo.description }}</p>
+              <br />
+            </div>
           </div>
         </div>
       </div>
