9e79a205010c87c690442c862f7c8eed0be83b27
Blanca Guiomar Castro <bgcg1709@gmail.com>
2022-04-07 20:28:33 +0200

    projecto

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@
         <h2 class="text-sd font-light italic text-white sm:text-xl md:text-3xl">
           "A room without books is like a body without a soul"
         </h2>
-        <div class="flex flex-col items-center justify-center gap-3 py-2">
+        <div class="flex items-center justify-center gap-3 py-2">
           <div
             class="border-b-2 border-dashed border-amber-200 pr-2 text-lg leading-loose tracking-wide text-white sm:text-2xl"
           >
@@ -35,39 +35,54 @@
       </div>
     </div>
 
-    <div class="flex flex-col items-center pb-4">
+    <div class="container mx-auto flex flex-col items-center bg-blue-200 p-4">
       <h3 class="italic text-amber-600">Find your wish book:</h3>
-      <div>
+      <div class="flex gap-1">
         <!-- ref pasarlo a <v-model> -->
         <input
           type="text"
-          class="mt-2 rounded-md border-2 border-amber-700 py-1 md:px-8"
-          placeholder="title or author"
-          ref="searchInput"
+          class="mt-2 rounded-md border-2 border-amber-700 py-1"
+          placeholder=" title or author"
+          v-model="searchInput"
           @keyup.enter="getBooks()"
         />
         <button
-          class="mt-3 rounded-md bg-amber-600 py-1 px-1 hover:bg-orange-400"
+          class="mt-3 rounded-md bg-orange-400 py-1 px-1 hover:bg-orange-400"
           @click="getBooks()"
         >
-          Q
+          jjjj
         </button>
       </div>
     </div>
 
     <div v-if="bookInfo">
-      <div class="container mx-auto grid grid-cols-3 gap-2 bg-gray-500">
+      <div
+        class="container mx-auto grid gap-2 bg-gray-500 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5"
+      >
         <div v-for="book in bookInfo.items" :key="book.id">
-          <div v-for="image in book.volumeInfo.imageLinks" :key="image">
-            <img :src="image" alt="books images" class="h-24" />
-            <div class="bg-orange-100 text-justify text-xs">
-              <p>{{ book.volumeInfo.title }}</p>
-              <div v-for="author in book.volumeInfo.authors" :key="author">
-                <p>autor: {{ author }}</p>
-              </div>
-              <p>description: {{ book.volumeInfo.description }}</p>
-              <br />
+          <img
+            v-if="
+              book.volumeInfo.imageLinks &&
+              book.volumeInfo.imageLinks.smallThumbnail
+            "
+            :src="book.volumeInfo.imageLinks.smallThumbnail"
+            alt="books images"
+            class="item-center h-56"
+          />
+          <img
+            v-else
+            src="@/images/nocoverimage.jpeg"
+            alt="books images"
+            class="h-56"
+          />
+          <div class="bg-orange-100 text-justify">
+            <p class="text-md font-bold md:text-lg">
+              {{ book.volumeInfo.title }}
+            </p>
+            <div v-for="author in book.volumeInfo.authors" :key="author">
+              <p class="text-md italic md:text-lg">{{ author }}</p>
             </div>
+            <br />
           </div>
         </div>
       </div>
@@ -77,24 +92,26 @@
 
 <script>
 import axios from "axios";
+
 export default {
   name: "Home",
   data() {
     return {
       bookInfo: null,
+      searchInput: null,
     };
   },
   components: {},
   methods: {
     async getBooks() {
-      if (this.$refs.searchInput.value === "") {
+      if (this.searchInput === "") {
         this.bookInfo = null;
         return;
       }
 
       try {
         const { data } = await axios(
-          `https://www.googleapis.com/books/v1/volumes?q=intitle:"${this.$refs.searchInput.value}"|inauthor:"${this.$refs.searchInput.value}"&maxResults=6`
+          `https://www.googleapis.com/books/v1/volumes?q=intitle:"${this.searchInput}"|inauthor:"${this.searchInput}"&maxResults=30`
         );
         this.bookInfo = data;
       } catch (error) {
